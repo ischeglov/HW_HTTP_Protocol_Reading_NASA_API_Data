a6a7cae0e7cb2596a4c91bbb0ac8f29861332e7a
ischeglov <Ivan_mix@mail.ru>
2023-10-22 21:27:49 +0300

    add basic logic and Astronomy Picture of the Day

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
+    implementation("org.apache.httpcomponents:httpclient:4.5.12")
 }
 
 tasks.test {
--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Launcher {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Launcher {
+
+    public static final String url = "https://api.nasa.gov/planetary/apod?api_key=ВАШ_КЛЮЧ";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
+
+        Nasa nasa = mapper.readValue(response.getEntity().getContent(), Nasa.class);
+
+        CloseableHttpResponse image = httpClient.execute(new HttpGet(nasa.getUrl()));
+        String[] nasaArray = nasa.getUrl().split("/");
+        String fileName = nasaArray[nasaArray.length - 1];
+
+        HttpEntity entity = image.getEntity();
+        if (entity != null) {
+            FileOutputStream fos = new FileOutputStream(fileName);
+            entity.writeTo(fos);
+            fos.close();
+        }
+
+        httpClient.close();
+        response.getEntity().getContent().close();
+    }
 }
